1afbda0bedcb78cc111d2515a78e7a1baa283a7f
kosimaru1997 <kosimaru1997@gmail.com>
2021-07-13 20:21:58 +0900

    memo

--- test.rb
+++ test.rb
@@ -1,2 +1,13 @@
-User.where(@hoge).class
-=> User::ActiveRecord_Relation
+Attribute.new(table, name)
+=> #<struct Arel::Attributes::Attribute
+ relation=
+  #<Arel::Table:0x00007f8da0abe868
+   @klass=
+    User(id: integer, name: string, created_at: datetime, updated_at: datetime),
+   @name="users",
+   @table_alias=nil,
+   @type_caster=
+    #<ActiveRecord::TypeCaster::Map:0x00007f8da0abe2f0
+     @klass=
+      User(id: integer, name: string, created_at: datetime, updated_at: datetime)>>,
+ name="nam">
